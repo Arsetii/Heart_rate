f8e2dfd645484a0ab351d0d055aa2596fef060c8
Arsen <tatarenkoarsen@gmail.com>
2019-11-20 13:41:44 +0200

    add modal window

--- js/script.js
+++ js/script.js
@@ -52,6 +52,45 @@ $(document).ready(function(){
         })
       };
 
-      toggleSlide('.catalog-item__link')
-      toggleSlide('.catalog-item__back')
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
+
+      // modal
+      
+      $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+
+      $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thx, #order').fadeOut('slow')
+      });
+
+
+      $('.button_mini').each(function(i) {
+        $(this).on('click', function(){
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+        });
+      });
+
+     
+
+      function validateForms(form) {
+        $(form).validate({
+          rules:{
+            name: "required",
+            phone: "required",
+            email: {
+              required: true,
+              email:true
+            }
+          }
+        });
+      }
+
+      validateForms('#consultation-form');
+      validateForms('#order form');
+      validateForms('#consultation form');
+
+      $('input[name=phone]').mask("+38 (999) 999-99-99");
   });
